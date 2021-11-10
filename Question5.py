a4d3908f129eb6ff5bf328a76677d01b96ea7863
YvanSamunani <90831373+YvanSamunani@users.noreply.github.com>
2021-11-10 19:41:19 +0200

    Update Question5.py

--- Question5.py
+++ Question5.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # List one
 
@@ -40,4 +36,3 @@ if count > 1:
     print("True")
 else:
     print("false")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
